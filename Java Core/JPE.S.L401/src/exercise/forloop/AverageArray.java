afdfbfdda0ac8fc21c409075cf364910387bae24
kienltcntt <kienlt.software@gmail.com>
2022-01-20 13:01:16 +0700

    Move to right directory.

---
+++ Java Core/JPE.S.L401/src/exercise/forloop/AverageArray.java
@@ -0,0 +1,15 @@
+package exercise.forloop;
+
+public class AverageArray {
+    public static void main(String[] args) {
+        int[] array = {12, 5, 9, 8, 11, 86};
+        double average;
+        int sum = 0;
+        for(int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        average = sum / array.length;
+        System.out.println("Average of all element in array: " + average);
+        //hmmm
+    }
+}
