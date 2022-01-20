5259afa95f4207b4f03e79a2f9e4c18d873e40a6
kienltcntt <kienlt.software@gmail.com>
2022-01-20 11:13:04 +0700

    LAB401

--- JPE.S.L401/src/exercise/ifelse/CheckPassFail.java
+++ JPE.S.L401/src/exercise/ifelse/CheckPassFail.java
@@ -1,4 +1,19 @@
 package exercise.ifelse;
 
+import java.util.Scanner;
+
 public class CheckPassFail {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter value : ");
+        int mark = sc.nextInt();
+
+        if(mark >= 50) {
+            System.out.println("PASS");
+        } else {
+            System.out.println("FAIL");
+        }
+
+        System.out.println("CHECK DONE");
+    }
 }
