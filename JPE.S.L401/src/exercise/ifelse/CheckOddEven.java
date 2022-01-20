838ac8e7159fe05b305c1b3af078a65ca1278ed0
kienltcntt <kienlt.software@gmail.com>
2022-01-20 11:23:31 +0700

    LAB401

--- JPE.S.L401/src/exercise/ifelse/CheckOddEven.java
+++ JPE.S.L401/src/exercise/ifelse/CheckOddEven.java
@@ -1,4 +1,19 @@
 package exercise.ifelse;
 
+import java.util.Scanner;
+
 public class CheckOddEven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter value for number: ");
+        int number = sc.nextInt(); sc.nextLine();
+        System.out.println("The number is : " + number);
+        if(number % 2 == 0) {
+            System.out.println("Odd Number");
+        } else {
+            System.out.println("Even Number");
+        }
+        System.out.println("----------");
+        System.out.println("Bye!");
+    }
 }
